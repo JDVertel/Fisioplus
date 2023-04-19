20804410991b65457e18c1448fa3b04953e603eb
vertelink <juandavidvertel@hotmail.com>
2023-04-18 20:44:37 -0500

    instalacion boostrap, axios y login base

---
+++ frontend/fisioterapia/src/main.js
@@ -0,0 +1,14 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.js'
+//axios
+import * as Vue from 'vue' // in Vue 3
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+
+
+import './assets/main.css'
+
+createApp(App).mount('#app')
