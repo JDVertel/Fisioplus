b45cbde9af22a055b7969f9e023e0ed8f0899d27
juan vertel <juandavidvertelh@gmail.com>
2023-04-25 11:23:02 -0500

    rutas separadas y trabajando, listo para continuar front

--- frontend/fisioterapia/src/main.js
+++ frontend/fisioterapia/src/main.js
@@ -1,5 +1,8 @@
-import "./assets/main.css";
 import { createApp } from "vue";
+//componentes-rutas
+import App from "./App.vue";
+import router from "./router/routes";
+
 //bootstrap
 import "bootstrap/dist/css/bootstrap.css";
 import bootstrap from "bootstrap/dist/js/bootstrap.bundle.js";
@@ -9,54 +12,4 @@ import * as Vue from "vue"; // in Vue 3
 import axios from "axios";
 import VueAxios from "vue-axios";
 
-//vue-router
-import { createRouter, createWebHashHistory } from "vue-router";
-
-
-/* ************************************************************************************** */
-//componentes-rutas
-import App from "./App.vue";
-import Home from "./components/home.vue";
-import About from "./components/about.vue";
-import Login from "./components/login.vue";
-import Register from "./components/Usuarios/register.vue"
-
-// agendas
-import A_parametros from "./components/Agendas/a_Params.vue"
-import A_programar from "./components/Agendas/a_Program.vue"
-import A_reservas from "./components/Agendas/a_Reservs.vue"
-//contenido web
-import servicios from "./components/contenido/C_Servicios.vue"
-import productos from "./components/contenido/C_Productos.vue"
-import actividades from "./components/contenido/C_Actividades.vue"
-
-
-
-//definir rutas
-const routes = [
-  { path: "/", component: Home },
-  { path: "/about", component: About },
-  { path: "/login", component: Login },
-  { path: "/register", component: Register },
-  // agendas
-  { path: "/A_parametros", component: A_parametros },
-  { path: "/A_programar", component: A_programar },
-  { path: "/A_reservas", component: A_reservas },
-//contenido web
-{ path: "/Servicios", component: servicios },
-{ path: "/productos", component: productos },
-{ path: "/Actividades", component: actividades },
-]
-
-
-/* ************************************************************************************** */
-//creamos objetos rutas de vue router
-
-const router = createRouter({
-  history: createWebHashHistory(),
-  routes,
-});
-/* ************************************************************************************** */
-const app = createApp(App)
-app.use(router)
-app.mount("#app")
+createApp(App).use(router).mount("#app");
--- frontend/fisioterapia/src/router/routes.js
+++ frontend/fisioterapia/src/router/routes.js
@@ -1,25 +1,38 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Hola from '@/components/Hola.vue'
-
-Vue.use(VueRouter)
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "./../components/home.vue";
+import About from "./../components/about.vue";
+import Login from "./../components/login.vue";
+import Register from "./../components/register.vue";
+// agendas
+import A_parametros from "./../components/Agendas/a_Params.vue";
+import A_programar from "./../components/Agendas/a_Program.vue";
+import A_reservas from "./../components/Agendas/a_Reservs.vue";
+//contenido web
+import servicios from "./../components/contenido/C_Servicios.vue";
+import productos from "./../components/contenido/C_Productos.vue";
+import actividades from "./../components/contenido/C_Actividades.vue";
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: Hola
-  }
-]
+  { path: "/", name: 'home',component: Home},
+  { path: "/about", component: About, name: "about" },
+  { path: "/login", component: Login, name: "login" },
+  { path: "/register", component: Register, name: "register" },
+  // agendas
+  { path: "/A_parametros", component: A_parametros, name: "parametros" },
+  { path: "/A_programar", component: A_programar, name: "programar" },
+  { path: "/A_reservas", component: A_reservas, name: "reservas" },
+  //contenido web
+  { path: "/Servicios", component: servicios, name: "servicios" },
+  { path: "/productos", component: productos, name: "productos" },
+  { path: "/Actividades", component: actividades, name: "actividades" },
+];
 
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
-})
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
 
-export default router
-
+export default router;
 
 /* 
 
