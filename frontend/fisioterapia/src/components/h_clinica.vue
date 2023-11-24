ab9b3a060a2bae3fa77cc03eca63a624424f9d07
vertelink <juandavidvertelh@gmail.com>
2023-11-24 16:35:24 -0500

    trabajando hc 4

--- frontend/fisioterapia/src/components/h_clinica.vue
+++ frontend/fisioterapia/src/components/h_clinica.vue
@@ -290,12 +290,12 @@
             </div>
         </div>
     </div>
-
+<!--  -->
     <div class="accordion-item">
         <h2 class="accordion-header">
             <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapse3" aria-expanded="false" aria-controls="panelsStayOpen-collapse3">
                 <v-icon name="io-construct-sharp" fill="red" scale="0.7" />
-                Inspeccion Dinamica
+               Exploracion Fisica
             </button>
         </h2>
         <div id="panelsStayOpen-collapse3" class="accordion-collapse collapse">
@@ -303,6 +303,142 @@
                 fase de apoyo fase de balanceo adaptaci1s del cuerpo durante la marcha
                 análisis cuantitativo de la marcha
             </div>
+
+            <nav>
+                <div class="nav nav-tabs" id="nav-tab" role="tablist">
+                  <button class="nav-link active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">Piel</button>
+                  <button class="nav-link" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">Lesiones</button>
+                  <button class="nav-link" id="nav-contact-tab" data-bs-toggle="tab" data-bs-target="#nav-contact" type="button" role="tab" aria-controls="nav-contact" aria-selected="false">Biotipo</button>
+                  <button class="nav-link" id="nav-disabled-tab" data-bs-toggle="tab" data-bs-target="#nav-disabled" type="button" role="tab" aria-controls="nav-disabled" aria-selected="false" >Postura</button>
+                </div>
+              </nav>
+              <div class="tab-content" id="nav-tabContent">
+                <div class="tab-pane fade show active m-3" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab" tabindex="0">
+                    <button type="button" class="btn btn-primary btn-sm">+Add</button>
+                    <table class="table table-sm">
+                        <thead>
+                          <tr>
+                            <th scope="col">Tipo</th>
+                            <th scope="col">Hallazgo</th>
+                            <th scope="col">Especificacion</th>
+               
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr>
+                            <td>Mark</td>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                  
+                          </tr>
+                          <tr>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>Mark</td>
+                          </tr>
+                          <tr>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>Mark</td>
+                          </tr>
+                        </tbody>
+                      </table>
+                </div>
+                <div class="tab-pane fade  m-3" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab" tabindex="0">
+                    <button type="button" class="btn btn-primary btn-sm">+Add</button>
+                    <table class="table table-sm">
+                        <thead>
+                          <tr>
+                            <th scope="col">Tipo</th>
+                            <th scope="col">Hallazgo</th>
+                            <th scope="col">Especificacion</th>
+               
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr>
+                            <td>Mark</td>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                  
+                          </tr>
+                          <tr>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>Mark</td>
+                          </tr>
+                          <tr>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>Mark</td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    
+                </div>
+                <div class="tab-pane fade  m-3" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab" tabindex="0">
+                    <button type="button" class="btn btn-primary btn-sm">+Add</button>
+                    <table class="table table-sm">
+                        <thead>
+                          <tr>
+                            <th scope="col">Tipo</th>
+                            <th scope="col">Hallazgo</th>
+                            <th scope="col">Especificacion</th>
+               
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr>
+                            <td>Mark</td>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                  
+                          </tr>
+                          <tr>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>Mark</td>
+                          </tr>
+                          <tr>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>Mark</td>
+                          </tr>
+                        </tbody>
+                      </table>
+                </div>
+                <div class="tab-pane fade  m-3" id="nav-disabled" role="tabpanel" aria-labelledby="nav-disabled-tab" tabindex="0">
+                    <button type="button" class="btn btn-primary btn-sm">+Add</button>
+                    <table class="table table-sm">
+                        <thead>
+                          <tr>
+                            <th scope="col">Tipo</th>
+                            <th scope="col">Hallazgo</th>
+                            <th scope="col">Especificacion</th>
+               
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr>
+                            <td>Mark</td>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                  
+                          </tr>
+                          <tr>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>Mark</td>
+                          </tr>
+                          <tr>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>Mark</td>
+                          </tr>
+                        </tbody>
+                      </table>
+                </div>
+              </div>
         </div>
     </div>
     <div class="accordion-item">
@@ -577,8 +713,8 @@
         </h2>
         <div id="panelsStayOpen-collapse11" class="accordion-collapse collapse">
             <div class="accordion-body">
-                <div class="display-6">guardar historia clincia</div>
-
+         
+                <button type="button" class="btn btn-primary btn-sm">guardar historia clinica</button>
             </div>
         </div>
 
