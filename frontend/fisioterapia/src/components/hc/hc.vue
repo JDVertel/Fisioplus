7cf04372b257bf6dc59f349203f2e01d58c74a34
juan david vertel holguin <127699117+JDVertel@users.noreply.github.com>
2024-08-21 15:49:27 +0000

    avance 21

--- frontend/fisioterapia/src/components/hc/hc.vue
+++ frontend/fisioterapia/src/components/hc/hc.vue
@@ -1,85 +1,93 @@
 <template>
-<div class="container mt-3">
+  <div class="container mt-3" id="panelbusqueda" v-if="registrado == 1">
+
+    <h1>paciente</h1>
+    <p>ID recibido: {{ iduser }}</p>
+
     <div class="row">
-        <h3 class="centrarcontenido">Historia Clinica</h3>
-  
+      <h3 class="centrarcontenido">Historia Clinica</h3>
     </div>
     <div class="container centrado">
-        <input type="text" class="form-control-sm textarea" id="search textarea" placeholder="Numero de Documento" />
-
-        <button class="btn btn-primary btn-sm" type="submit">Buscar</button>
+      <input type="text" class="form-control-sm textarea" id="search textarea" placeholder="Numero de Documento" />
+      <button class="btn btn-primary btn-sm" type="submit">Buscar</button>
     </div>
-</div>
+  </div>
 
-<hr />
+  <hr />
 
-<!-- **************************************************************************************************** -->
-<div id="register" v-if="registrado==0">
+  <!-- **************************************************************************************************** -->
+  <div id="register" v-if="registrado == 0">
     <h6 class="display-6 text-center">Paciente no Encontrado !!!</h6>
 
     <Registro />
+  </div>
 
-</div>
-
-<!-- **************************************************************************************************** -->
-<div class="container-fluid Cont_hc">
-
-
-
+  <!-- **************************************************************************************************** -->
+  <div class="container-fluid Cont_hc">
     <div class="row">
-            <div class="col-3 col-md-2 centrarcontenido">
-                <img src="./../../assets/images/usuario.jpg" class="imagenhc" alt="..." />
+      <div class="col-3 col-md-2 centrarcontenido">
+        <img src="./../../assets/images/usuario.jpg" class="imagenhc" alt="..." />
+      </div>
+
+      <div class="col-9 col-md-10">
+        <div class="container mt-3" style="padding: 25px">
+          <h6>
+            <strong> Datos Personales</strong>
+          </h6>
+          <div class="row">
+            <div class="col-12 col-md-3">
+              <strong>Nombres:</strong> juan david
             </div>
-        
-            <div class="col-9 col-md-10">
-                <div class="container mt-3" style="padding: 25px;">
-                    <h6>
-                        <strong>
-                          Datos
-                            Personales</strong>
-                    </h6>
-                    <div class="row">
-                        <div class="col-12 col-md-3"><strong>Nombres:</strong> juan david </div>
-                        <div class="col-12 col-md-3"><strong>Apellidos:</strong> vertel holguin</div>
-                        <div class="col-12 col-md-3"><strong>Sexo:</strong> M</div>
-                        <div class="col-12 col-md-3"><strong>Edad:</strong> 43</div>
-                        <div class="col-12 col-md-3"><strong>TipoDoc:</strong> CC</div>
-                        <div class="col-12 col-md-3"><strong>Numdoc:</strong> 13862306 </div>
-                        <div class="col-12 col-md-3"><strong>Tipo Sangre :</strong> A+</div>
-                        <div class="col-12 col-md-3"><strong>E Civil:</strong> U libre</div>
-                        <div class="col-12 col-md-3"><strong>Escolaridad:</strong> maestria </div>
-                        <div class="col-12 col-md-3"><strong>Ocupacion:</strong> ingeniero de  sistemas</div>
-                        <div class="col-12 col-md-3"><strong>Direccion:</strong> calle 52a # 20-38 torcoroma</div>
-                        <div class="col-12 col-md-3"><strong>Celular:</strong> 3145563439</div>
-                    </div>
-                </div>
+            <div class="col-12 col-md-3">
+              <strong>Apellidos:</strong> vertel holguin
             </div>
-      
+            <div class="col-12 col-md-3"><strong>Sexo:</strong> M</div>
+            <div class="col-12 col-md-3"><strong>Edad:</strong> 43</div>
+            <div class="col-12 col-md-3"><strong>TipoDoc:</strong> CC</div>
+            <div class="col-12 col-md-3"><strong>Numdoc:</strong> 13862306</div>
+            <div class="col-12 col-md-3"><strong>Tipo Sangre :</strong> A+</div>
+            <div class="col-12 col-md-3"><strong>E Civil:</strong> U libre</div>
+            <div class="col-12 col-md-3">
+              <strong>Escolaridad:</strong> maestria
+            </div>
+            <div class="col-12 col-md-3">
+              <strong>Ocupacion:</strong> ingeniero de sistemas
+            </div>
+            <div class="col-12 col-md-3">
+              <strong>Direccion:</strong> calle 52a # 20-38 torcoroma
+            </div>
+            <div class="col-12 col-md-3">
+              <strong>Celular:</strong> 3145563439
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    <br>
+    <br />
     <C_Hc />
 
-    <br>
+    <br />
+  </div>
 
-</div>
-
-    <v-icon name="fa-home" fill="blue" /><router-link to="/dashboard">Home</router-link>
+  <v-icon name="fa-home" fill="blue" /><router-link to="/dashboard">Home</router-link>
 </template>
 
 <script>
 import HCdetallada from "./h_clinica.vue";
 
-import Registro from "./registroForm.vue"
+import Registro from "./registroForm.vue";
 
 export default {
-    data: () => ({
-        registrado: 1,
-    }),
+  data: () => ({
+    registrado: 1,
+  }),
 
-    components: {
-        C_Hc: HCdetallada,
-    
-    }
-
-}
+  components: {
+    C_Hc: HCdetallada,
+  },
+  created() {
+    // Recuperar el parámetro 'id' de la ruta
+    this.iduser = this.$route.params.idpaciente;
+  }
+};
 </script>
--- frontend/fisioterapia/src/components/profesional/index.vue
+++ frontend/fisioterapia/src/components/profesional/index.vue
@@ -1,123 +1,132 @@
 <template>
-<div class="container-fluid">
+    <div class="container-fluid">
 
-    <!--  pagina de profesional
+        <!--  pagina de profesional
     llegan ( idprofesional - id_ips- tipoagenda)
     se cargan de entrada citas del profesional en el dia actual
     renderiza tabla y cada registro debe tener el campo de asistir y opcion de llenar hc usar el store de AUTH para registro y el de HC Para el resto de hc -->
 
-    <div class="card w-100 mt-3 mb-3">
-        <div class="card-body">
-            <h5 class="card-title centrado">Agenda del Dia </h5>
-            <div class="row">
-                <div class="col-2">
-                    <img src="..." class="img-thumbnail centrado" alt="...">
-                </div>
-                <div class="col-10">
+        <div class="card w-100 mt-3 mb-3">
+            <div class="card-body">
+                <h5 class="card-title centrado">Agenda del Dia </h5>
+                <div class="row">
+                    <div class="col-2">
+                        <img src="..." class="img-thumbnail centrado" alt="...">
+                    </div>
+                    <div class="col-10">
 
-                    <p>profesional: Ramon jose vertel </p>
+                        <p>profesional: Ramon jose vertel </p>
 
-                    <div class="row">
-                        <div class="col-6 col-md-3">
-                            <p>Servicio: consulta </p>
-                        </div>
-                        <div class="col-6 col-md-3">
-                            <p>Fecha: {{this.fechaHoy}} </p>
-                        </div>
-                        <div class="col-6 col-md-3">
-                            <p>Ips: rehabilitacion erika jasmin </p>
-                        </div>
-                        <div class="col-6 col-md-3">
-                            <p>Nit: 123456 </p>
+                        <div class="row">
+                            <div class="col-6 col-md-3">
+                                <p>Servicio: consulta </p>
+                            </div>
+                            <div class="col-6 col-md-3">
+                                <p>Fecha: {{ this.fechaHoy }} </p>
+                            </div>
+                            <div class="col-6 col-md-3">
+                                <p>Ips: rehabilitacion erika jasmin </p>
+                            </div>
+                            <div class="col-6 col-md-3">
+                                <p>Nit: 123456 </p>
+                            </div>
                         </div>
                     </div>
-                </div>
 
+                </div>
+            </div>
+        </div>
+        <div class="card w-100 mb-3">
+            <div class="card-body">
+                <h5 class="card-title">Citas</h5>
+                <p class="card-text">Pacientes Agendados el dia de hoy </p>
+
+                <table class="table table-striped table-sm ">
+                    <thead class="table-dark">
+                        <tr>
+                            <th scope="col">Hora</th>
+                            <th scope="col">Paciente</th>
+                            <th scope="col">Celular</th>
+                            <th scope="col">Asistencia</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="cita in this.CitasAgendadas" :key="cita.id">
+                            <td>{{ cita.hora }}</td>
+                            <td>{{ cita.paciente }}</td>
+                            <td>{{ cita.telpaciente }}</td>
+                            <td><button type="button" class="btn btn-danger btn-sm"
+                                    @click="ActualizaEstadoCita('NO', cita)">X</button>
+                                <router-link :to="{ name: 'hc', params: { idpaciente: cita.numdoc } }">
+                                    <button type="button" class="btn btn-primary btn-sm"
+                                        @click="ActualizaEstadoCita('SI', cita)">OK</button>
+                                </router-link>
+
+
+                            </td>
+                        </tr>
+
+                    </tbody>
+                </table>
+                <!--             <a href="#" class="btn btn-primary">Button</a> -->
+            </div>
+        </div>
+
+        <p class="d-inline-flex gap-1">
+            <a class="btn btn-primary" data-bs-toggle="collapse" href="#collapseExample" role="button"
+                aria-expanded="false" aria-controls="collapseExample">
+                Historial diario
+            </a>
+
+        </p>
+        <div class="collapse" id="collapseExample">
+            <div class="card card-body">
+                <caption>Citas Asistidas del dia </caption>
+                <table class="table table-striped table-sm ">
+                    <thead class="table-info">
+                        <tr>
+                            <th scope="col">Hora</th>
+                            <th scope="col">Paciente</th>
+                            <th scope="col">Celular</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="cita in this.CitasAsistidas" :key="cita.id">
+                            <td>{{ cita.hora }}</td>
+                            <td>{{ cita.paciente }}</td>
+                            <td>{{ cita.telpaciente }}</td>
+
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+            <div class="card card-body">
+                <caption>Citas NO Asistdas del dia </caption>
+                <table class="table table-striped table-sm ">
+
+                    <thead class="table-danger">
+                        <tr>
+                            <th scope="col">Hora</th>
+                            <th scope="col">Paciente</th>
+                            <th scope="col">Celular</th>
+
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="cita in this.CitasNOAsistidas" :key="cita.id">
+                            <td>{{ cita.hora }}</td>
+                            <td>{{ cita.paciente }}</td>
+                            <td>{{ cita.telpaciente }}</td>
+
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
-    <div class="card w-100 mb-3">
-        <div class="card-body">
-            <h5 class="card-title">Citas</h5>
-            <p class="card-text">Pacientes Agendados el dia de hoy </p>
-
-            <table class="table table-striped table-sm ">
-                <thead class="table-dark">
-                    <tr>
-                        <th scope="col">Hora</th>
-                        <th scope="col">Paciente</th>
-                        <th scope="col">Celular</th>
-                        <th scope="col">Asistencia</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="cita in this.CitasAgendadas" :key="cita.id">
-                        <td>{{cita.hora}}</td>
-                        <td>{{cita.paciente}}</td>
-                        <td>{{cita.telpaciente}}</td>
-                        <td><button type="button" class="btn btn-danger btn-sm" @click="ActualizaEstadoCita('NO',cita)">X</button><button type="button" class="btn btn-primary btn-sm" @click="ActualizaEstadoCita('SI',cita)">OK</button></td>
-                    </tr>
-
-                </tbody>
-            </table>
-            <!--             <a href="#" class="btn btn-primary">Button</a> -->
-        </div>
-    </div>
-
-    <p class="d-inline-flex gap-1">
-        <a class="btn btn-primary" data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
-            Historial diario
-        </a>
-
-    </p>
-    <div class="collapse" id="collapseExample">
-        <div class="card card-body">
-            <caption>Citas Asistidas del dia </caption>
-            <table class="table table-striped table-sm ">
-                <thead class="table-info">
-                    <tr>
-                        <th scope="col">Hora</th>
-                        <th scope="col">Paciente</th>
-                        <th scope="col">Celular</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="cita in  this.CitasAsistidas" :key="cita.id" >
-                        <td>{{cita.hora}}</td>
-                        <td>{{cita.paciente}}</td>
-                        <td>{{cita.telpaciente}}</td>
-               
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-
-        <div class="card card-body">
-            <caption>Citas NO Asistdas del dia </caption>
-            <table class="table table-striped table-sm ">
-           
-                <thead class="table-danger">
-                    <tr>
-                        <th scope="col">Hora</th>
-                        <th scope="col">Paciente</th>
-                        <th scope="col">Celular</th>
-            
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="cita in  this.CitasNOAsistidas" :key="cita.id" >
-                        <td>{{cita.hora}}</td>
-                        <td>{{cita.paciente}}</td>
-                        <td>{{cita.telpaciente}}</td>
-               
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-    </div>
-</div>
-<br>
-<br>
+    <br>
+    <br>
 
 </template>
 
@@ -129,8 +138,8 @@ import {
 import moment from "moment";
 
 export default {
-    components:{
-    
+    components: {
+
     },
     data: () => ({
         fechaHoy: "",
@@ -157,9 +166,9 @@ export default {
             await this.getDatabyParam(this.paramsGetAllcitas)
 
             this.CitasAsistidas = this.dataCitas.filter(cita => cita.estado == "SI");
-            this.CitasNOAsistidas =  this.dataCitas.filter(cita => cita.estado == "NO");
-            this.CitasAgendadas =  this.dataCitas.filter(cita => cita.estado == "0");
-          
+            this.CitasNOAsistidas = this.dataCitas.filter(cita => cita.estado == "NO");
+            this.CitasAgendadas = this.dataCitas.filter(cita => cita.estado == "0");
+
         },
 
         fijarfechadia() {
@@ -180,14 +189,21 @@ export default {
                 telpaciente: Dcita.telpaciente,
                 tip: Dcita.tipo,
                 bd: "citas",
-            }]
+            }];
+
+            if (rta == "SI") {
+                console.log("cita asistida");
+            } else {
+                console.log("cita no asistida");
+            };
+
             await this.updateReserva(this.ParamsActualizarCita[0])
             this.GetAllCitasDia();
         }
 
     },
     computed: {
-        ...mapState('Agendas', ['dataCitas', ]),
+        ...mapState('Agendas', ['dataCitas',]),
         /*     ...mapState('Auth', []),
 
  */
